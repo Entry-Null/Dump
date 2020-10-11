ef16c39b429d67fcb6f59d4369b5c937766e317a
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2020-10-11 13:45:37 -0500

    Update LuaString.lua

--- LuaString.lua
+++ LuaString.lua
@@ -1,4 +1,5 @@
 local IsExploit = true
 local IsPaid = true
 local IsDebug = false
+local CopyWhenDone = true
 loadstring(game:HttpGet("https://github.com/Entry-Null/Dump/blob/main/LuaScriptDump.lua"))()
